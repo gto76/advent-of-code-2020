c827f3cd709ee79f7f727a9d4295a5c49262a96a
Jure Šorn <sornjure@gmail.com>
2020-12-11 22:03:21 +0100

    Working on problems

--- advent_2020.py
+++ advent_2020.py
@@ -231,32 +231,8 @@ b'''
 def problem_6_a(lines):
     '''For each group, count the number of questions to which anyone answered "yes". What is
     the sum of those counts? 11'''
-    groups = ' '.join(lines).split('  ')
-    return sum(len(set(group) - {' '}) for group in groups)
-
-
-# def problem_6_a(lines):
-#     '''For each group, count the number of questions to which anyone answered "yes". What is
-#     the sum of those counts? 11'''
-#     groups = ' '.join(lines).split('  ')
-#     count_groups_answers = lambda group: len(set(group.replace(' ', '')))
-#     return sum(count_groups_answers(group) for group in groups)
-
-
-# def problem_6_a(lines):
-#     '''For each group, count the number of questions to which anyone answered "yes". What is
-#     the sum of those counts? 11'''
-#     groups = ' '.join(lines).split('  ')
-#     get_groups_answers = lambda group: set(group.replace(' ', ''))
-#     return sum(len(get_groups_answers(group)) for group in groups)
-
-
-# def problem_6_a(lines):
-#     '''For each group, count the number of questions to which anyone answered "yes". What is
-#     the sum of those counts? 11'''
-#     groups = ' '.join(lines).split('  ')
-#     get_groups_answers = lambda group: set(group) - {' '}
-#     return sum(len(get_groups_answers(group)) for group in groups)
+    groups = (set(group) - {' '} for group in ' '.join(lines).split('  '))
+    return sum(len(group) for group in groups)
 
 
 def problem_6_b(lines):
@@ -264,40 +240,132 @@ def problem_6_b(lines):
     the sum of those counts? 6'''
     import functools, operator
     groups             = ' '.join(lines).split('  ')
-    split_group        = lambda group: [set(a) for a in group.split(' ')]
+    split_group        = lambda group: (set(a) for a in group.split(' '))
     get_common_answers = lambda group: functools.reduce(operator.and_, split_group(group))
     return sum(len(get_common_answers(group)) for group in groups)
 
 
-# def problem_6_b(lines):
-#     '''For each group, count the number of questions to which everyone answered "yes". What is
-#     the sum of those counts? 6'''
-#     import functools, operator
-#     groups         = ' '.join(lines).split('  ')
-#     get_people     = lambda group: [set(a) for a in group.split(' ')]
-#     common_answers = [functools.reduce(operator.and_, get_people(group)) for group in groups]
-#     return sum(len(a) for a in common_answers)
+###
+##  DAY 7
+#
+
+IN_7 = \
+'''light red bags contain 1 bright white bag, 2 muted yellow bags.
+dark orange bags contain 3 bright white bags, 4 muted yellow bags.
+bright white bags contain 1 shiny gold bag.
+muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
+shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+dark olive bags contain 3 faded blue bags, 4 dotted black bags.
+vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
+faded blue bags contain no other bags.
+dotted black bags contain no other bags.'''
 
 
-# def problem_6_b(lines):
-#     '''For each group, count the number of questions to which everyone answered "yes". What is
-#     the sum of those counts? 6'''
-#     import functools, operator
-#     groups = ' '.join(lines).split('  ')
-#     split_group        = lambda group: [set(a) for a in group.split(' ')]
-#     get_common_answers = lambda group: functools.reduce(operator.and_, split_group(group))
-#     return sum(len(get_common_answers(group)) for group in groups)
+def problem_7_a(lines):
+    '''How many bag colors can eventually contain at least one shiny gold bag? 4'''
+    import re
+    def parse_line(line):
+        container, contents = line.split(' bags contain ')
+        if contents == 'no other bags.':
+            return container, set()
+        get_color = lambda token: re.search('\d+ (.*) .*$', token).group(1) 
+        return container, {get_color(a) for a in contents.split(',')}
+    bags = dict(parse_line(line) for line in lines)
+    out = {k for k, v in bags.items() if 'shiny gold' in v}
+    while True:
+        new_colors = {k for k, v in bags.items() if out & v}
+        if new_colors <= out:
+            return len(out)
+        out |= new_colors
 
 
-# def problem_6_b(lines):
-#     '''For each group, count the number of questions to which everyone answered "yes". What is
-#     the sum of those counts? 6'''
-#     import functools, operator
-#     groups = ' '.join(lines).split('  ')
-#     split_group = lambda group: [set(a) for a in group.split(' ')]
-#     get_common_answers = lambda group: functools.reduce(operator.and_, split_group(group))
-#     return sum(len(get_common_answers(group)) for group in groups)
+def problem_7_b(lines):
+    '''How many individual bags are required inside your single shiny gold bag? 32'''
+    import re
+    def parse_line(line):
+        container, contents = line.split(' bags contain ')
+        if contents == 'no other bags.':
+            return container, set()
+        get_number_and_color = lambda token: re.search('(\d+) (.*) .*$', token).groups()
+        return container, {get_number_and_color(a) for a in contents.split(',')}
+    def get_n_bags(color):
+        contents = bags[color]
+        if not contents:
+            return 1
+        return 1 + sum(int(n) * get_n_bags(color) for n, color in contents)
+    bags = dict(parse_line(line) for line in lines)
+    return get_n_bags('shiny gold') - 1
 
+
+###
+##  DAY 8
+#
+
+IN_8 = \
+'''nop +0
+acc +1
+jmp +4
+acc +3
+jmp -3
+acc -99
+acc +1
+jmp -4
+acc +6'''
+
+
+def problem_8_a(lines):
+    '''Run your copy of the boot code. Immediately before any instruction is executed a second
+    time, what value is in the accumulator? 5'''
+    OPERATIONS = dict(
+        acc=lambda pc, accumulator, argument: (pc+1, accumulator+int(argument)),
+        jmp=lambda pc, accumulator, argument: (pc+int(argument), accumulator),
+        nop=lambda pc, accumulator, argument: (pc+1, accumulator)
+    )
+    pc = 0
+    accumulator = 0
+    executed = set()
+    while pc not in executed:
+        executed.add(pc)
+        operation, argument = lines[pc].split()
+        pc, accumulator = OPERATIONS[operation](pc, accumulator, argument)
+    return accumulator
+
+
+def problem_8_b(lines):
+    '''Fix the program so that it terminates normally by changing exactly one jmp (to nop) or 
+    nop (to jmp). What is the value of the accumulator after the program terminates? 8'''
+    def main():
+        for program in program_generator():
+            result = run(program)
+            if result is not None:
+                return result
+
+    def program_generator():
+        for line_number in range(len(lines)):
+            while lines[line_number].startswith('acc'):
+                line_number += 1
+            line = lines[line_number]
+            new_line = line.replace('jmp', 'nop') if 'jmp' in line else line.replace('nop', 'jmp')
+            yield lines[:line_number] + [new_line] + lines[line_number+1:]
+
+    def run(program):
+        OPERATIONS = dict(
+            acc=lambda pc, accumulator, argument: (pc+1, accumulator+int(argument)),
+            jmp=lambda pc, accumulator, argument: (pc+int(argument), accumulator),
+            nop=lambda pc, accumulator, argument: (pc+1, accumulator)
+        )
+        pc = 0
+        accumulator = 0
+        executed = set()
+        while pc not in executed:
+            executed.add(pc)
+            operation, argument = program[pc].split()
+            pc, accumulator = OPERATIONS[operation](pc, accumulator, argument)
+            if pc == len(program):
+                return accumulator
+        return None
+
+    return main()
 
 
 # ###
@@ -310,12 +378,10 @@ def problem_6_b(lines):
 #
 # def problem_X_a(lines):
 #     ''''''
-#     pass
 #
 #
 # def problem_X_b(lines):
 #     ''''''
-#     pass
 
 
 ###
